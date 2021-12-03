acc7d84bd55ac92f483996622c67dc8e16926afa
Abel Garcia <a.garcia@aragonphotonics.com>
2021-12-03 09:10:35 +0100

    Paginas de confirmacion de email

---
+++ Pages/confirmed_email_page.py
@@ -0,0 +1,33 @@
+from dash import html, dcc
+from Pages.header import Header
+import Configuration.ReaderConfSystem as SysConfig
+from dash.dependencies import Input, Output, State
+
+
+layout = html.Div(
+    [Header(SysConfig.APP),
+     html.Div(
+     [
+         dcc.Location(id='url_forbidden_page', refresh=True),
+         html.H1(["Email confirmation accepted"], style={'color': 'blue'}),
+         html.Div(html.Button('Go back', id='submit-forbidden', n_clicks=0)
+                  , className='margin-10px'),
+     ],
+     className="row text-align-center margin-10px",
+     ),
+     ],
+    className="page",
+)
+
+
+@SysConfig.APP.callback(
+    Output(component_id='url_forbidden_page', component_property='pathname'),
+    [Input(component_id='submit-forbidden', component_property='n_clicks')]
+)
+def output_waiting(n_clicks):
+    if n_clicks > 0:
+        print('Hace algo en waiting page')
+        return '/sign_in_page'
+    else:
+        print('Pasa')
+
